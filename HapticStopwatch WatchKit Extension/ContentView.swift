c6838ffe2cb8324de2ca2d22bd5dabe5c94fb23b
Joshua Heslin <josh@cloutly.com>
2022-08-28 14:31:18 +0100

    Initial Commit

---
+++ HapticStopwatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HapticStopwatch WatchKit Extension
+//
+//  Created by Josh Heslin on 28/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HapticStopwatch WatchKit Extension/HapticStopwatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HapticStopwatchApp.swift
+//  HapticStopwatch WatchKit Extension
+//
+//  Created by Josh Heslin on 28/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct HapticStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
